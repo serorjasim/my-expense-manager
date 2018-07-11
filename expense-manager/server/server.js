b63901806cb4e5463e5521952c1311a111dfcc31
seror <seror94ab@yahoo.com>
2018-07-10 22:07:34 -0500

    added the CRUD routes

--- expense-manager/server/server.js
+++ expense-manager/server/server.js
@@ -1,12 +1,19 @@
-//server/server.js
 var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+
+mongoose.connect('mongodb://<serorjasim>:<soccerball1>@ds231951.mlab.com:31951/expenses_db');
+
+
 app.use('/', router);
 
 module.exports = app;
